f66a3230c0b42537b9969dfc78c41e783d7e629d
Gustavo Vargas Miranda <gvmgust@gmail.com>
2025-09-19 22:26:06 -0400

    Fix Prices plan

--- src/hooks/useCalculator.js
+++ src/hooks/useCalculator.js
@@ -14,41 +14,32 @@ const useCalculator = () => {
 		const computadoras = parseInt(computadorasInstalar) || 0;
 		const oficinas = parseInt(cantidadOficinas) || 0;
 		const ventasParsed = parseInt(ventas) || 0;
-		const promVentas =
-			ventasParsed *
-			(computadoras <= 2
-				? 1
-				: computadoras <= 4
-				? 0.9
-				: computadoras <= 8
-				? 0.8
-				: 0.7);
 
 		// Calcular puntuación total basada en pesos
 		const puntuacionTotal =
-			promVentas * 0.5 + usuarios * 2 + oficinas * 50 + computadoras * 25;
+			ventasParsed * 0.5 + // Importacia Ventas
+			usuarios * 2 + // Importancia Usuarios
+			oficinas * 50 + // Importancia Oficinas
+			computadoras * 25; // Importancia Computadoras
 
 		let plan = null;
 
 		// Determinar plan basado en la puntuación total
-		if (puntuacionTotal >= 0 && puntuacionTotal < 125) {
+		if (puntuacionTotal >= 0 && puntuacionTotal <= 125) {
 			plan = { nombre: "Básico", precio: 350 };
-		} else if (puntuacionTotal >= 125 && puntuacionTotal < 240) {
+		} else if (puntuacionTotal > 125 && puntuacionTotal <= 250) {
 			plan = { nombre: "Pyme", precio: 400 };
-		} else if (puntuacionTotal >= 240 && puntuacionTotal < 520) {
+		} else if (puntuacionTotal > 250 && puntuacionTotal <= 500) {
 			plan = { nombre: "Medium", precio: 500 };
-		} else if (puntuacionTotal >= 520 && puntuacionTotal <= 760) {
+		} else if (puntuacionTotal > 500 && puntuacionTotal <= 1000) {
 			plan = { nombre: "Plus", precio: 750 };
-		} else if (puntuacionTotal > 760 && puntuacionTotal <= 980) {
+		} else if (puntuacionTotal > 1000 && puntuacionTotal <= 1500) {
 			plan = { nombre: "PlusMax", precio: 1500 };
-		} else if (puntuacionTotal > 980 && puntuacionTotal <= 1200) {
+		} else if (puntuacionTotal > 1500 && puntuacionTotal <= 2000) {
 			plan = { nombre: "PlusSuper", precio: 2500 };
-		} else if (puntuacionTotal > 1200) {
+		} else if (puntuacionTotal > 2000) {
 			plan = { nombre: "Enterprise", precio: "Negociado" };
 		}
-
-		console.log(formData);
-		console.log(puntuacionTotal);
 
 		// Si no coincide con ningún plan, asignar por defecto Básico
 		if (!plan) {
